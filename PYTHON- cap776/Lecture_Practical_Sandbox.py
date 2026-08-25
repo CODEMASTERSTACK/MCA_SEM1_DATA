18c85feab2e8999a812ff42dadd76561df1ca679
unknown <kanasingh974@gmail.com>
2026-08-25 20:57:02 +0530

    Making the updates

---
+++ PYTHON- cap776/Lecture_Practical_Sandbox.py
@@ -0,0 +1,4 @@
+def spending( afternoon, evening, morning = 10):
+    print(f"Your spending of Morning is {morning}, \nSpending for afternoon is: {afternoon}, \nSpending for evening is: {evening}")
+
+spending(450, 500);
